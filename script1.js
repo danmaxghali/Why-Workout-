f840a63f128f1475461f01067bc2c0350086c618
danmaxghali <danmaxghali@gmail.com>
2024-11-12 13:52:09 +0000

    Initial commit

---
+++ script1.js
@@ -0,0 +1,8 @@
+// Get the button element
+const startBtn = document.getElementById("start-btn");
+
+// Add an event listener to the button
+startBtn.addEventListener("click", () => {
+    // Change the window's location to the next HTML page
+    window.location.href = "load1.html";
+});
